ff475029d94abb5babab242fca216ada3afa658c
sqspot <i20110323@gmail.com>
2024-06-22 21:05:22 +0800

    up

--- YT.js
+++ YT.js
@@ -1,6 +1,4 @@
 {
-//Type 0-xml、 1-json 、2-爬虫源 、3-自定义爬虫 、4-服务器爬虫。
-//"spider":"./custom_spider.jar",
 "spider":"./pg.jar",
 "lives":[
 {
@@ -46,9 +44,9 @@
 }
 ],
 "sites" : [
-{"key":"Youtube","name":"🛗Youtube⏩","type":3,"api":"csp_Youtube","searchable":1,"quickSearch":0,"changeable":0,"ext":{"json":"./youtube.json","type":"直播#新聞#劇集#電影#綜藝#紀錄片#音樂#體育#動物#風光#放鬆#4K#HDR#movie#music#documentary#bbcdocumentary#nationalgeographicdocumentary","keywords":"排行榜,HOT,TRENDS,熱門話題,熱門趨勢,熱門綜藝,熱門電影,熱門電視劇,小姐姐","codecs":""},"style":{"type":"rect","ratio":1.5},"jar":"./pg.jar"},
-{"key":"Youtube2","name":"👉🔥Youtube台灣新聞🎯👈","type":3,"api":"csp_Youtube","searchable":0,"quickSearch":0,"changeable":0,"ext":{"json":"./youtube2.json","codecs":""},"style":{"type":"rect","ratio":1.5},"jar":"./pg.jar"},
-{"key":"Youtube3","name":"🛗🎯Youtube測試⏩","type":3,"api":"csp_Youtube","searchable":0,"quickSearch":0,"changeable":0,"ext":{"json":"./youtube3.json","codecs":""},"style":{"type":"rect","ratio":1.5},"jar":"./pg.jar"},
+{"key":"Youtube","name":"🛗Youtube⏩","type":3,"api":"csp_Youtube","searchable":1,"quickSearch":0,"changeable":0,"ext":{"json":"./youtube.json","type":"直播#新聞#劇集#電影#綜藝#紀錄片#音樂#體育#動物#風光#放鬆#4K#HDR#movie#music#documentary#bbcdocumentary#nationalgeographicdocumentary","keywords":"排行榜,HOT,TRENDS,熱門話題,熱門趨勢,熱門綜藝,熱門電影,熱門電視劇,小姐姐","codecs":""},"style":{"type":"rect","ratio":1.5}},
+{"key":"Youtube2","name":"👉🔥Youtube台灣新聞🎯👈","type":3,"api":"csp_Youtube","searchable":0,"quickSearch":0,"changeable":0,"ext":{"json":"./youtube2.json","codecs":""},"style":{"type":"rect","ratio":1.5}},
+{"key":"Youtube3","name":"🛗🎯Youtube測試⏩","type":3,"api":"csp_Youtube","searchable":0,"quickSearch":0,"changeable":0,"ext":{"json":"./youtube3.json","codecs":""},"style":{"type":"rect","ratio":1.5}},
 {"key":"ikanbotsq","name":"🈵爱看机器人sq","type":3,"api":"./lib/drpy2.min.js","ext":"./js/ikanbotsq.js"}
 ],
 "wallpaper":"./黑色.jpeg",
