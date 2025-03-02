fbbe3a7ed42701fa53b4d5bce0ea5fc3aff975f0
sqspot <i20110323@gmail.com>
2025-03-02 20:32:01 +0800

    up

---
+++ js/jable.js
@@ -0,0 +1,19 @@
+{
+    "直接播放": "1",
+    "搜索模式": "1",
+    "搜索url": "https://jable.tv/search/{wd}/",
+    "数组": "<div class=\"img-box cover-md\">&&</h6>",
+    "图片": "data-src=\"&&\"",
+    "标题": "class=\"title\">&&</a>",
+    "链接": "href=\"&&\"",
+    "链接前缀": "https://jable.tv",
+    "副标题": "<span class=\"label\">&&</span>",
+    "播放数组": "<h6&&</h6>",
+    "播放列表": "<a&&/a>",
+    "播放标题": ">&&<",
+    "播放链接": "href=\"&&\"",
+    "分类": "最近更新$latest-updates#最新上市$new-release#主奴调教$categories/bdsm#字幕$categories/chinese-subtitle#凌辱$categories/rape#制服$categories/uniform#角色$categories/roleplay#偷拍$categories/hidden-cam#无码解码$categories/uncensored#多P$categories/groupsex#丝袜$categories/pantyhose",
+    "分类url": "https://jable.tv/{cateId}/{catePg}/",
+    "免嗅": "0",
+    "嗅探词": ".m3u8#.mp4"
+}
