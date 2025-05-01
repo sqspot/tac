d2f80d1f179390ce330e4dbee11533c8b8878e57
sqspot <i20110323@gmail.com>
2025-05-01 22:31:42 +0800

    up

--- ktv.js
+++ ktv.js
@@ -9,7 +9,7 @@
 //{"key":"MV_vod","name":"MTV合集","type":1,"api":"https://tvbox-mv--leosam2048.repl.co/mv/vod","searchable": 1,"quickSearch": 1,"filterable": 1,"playerType":"1"},
 //{"key": "MV_vod2","name": "🎸┃明星-接口2┃MV","type": 1,"api": "https://mv.饭太硬.ml/mv/vod","searchable": 1,"quickSearch": 1,"filterable": 1},
 //{"key":"Iktv","name":"KTV","type":3,"api":"csp_Iktv","searchable":1,"changeable":0,"playerType":"1"},
-{"key": "Iktv2","name": "🎤KTV┃音乐┃MV","type": 3,"api": "csp_WexIktv","searchable": 1,"changeable": 0,"jar": "./wex.txt"},
+{"key": "Iktv2","name": "🎤KTV┃音乐┃MV","type": 3,"api": "csp_WexIktv","searchable": 1,"changeable": 0},
 {"key":"Yinyuetai","name":"音悦台","type":3,"api":"csp_Yinyuetai","searchable":1,"changeable":0,"playerType":"1"}
 ]
 
